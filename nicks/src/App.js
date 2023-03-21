fc2379e1e28cf7550df614f14ca6ea6184116971
NicholasHM05 <nhmamberger@gmail.com>
2023-03-21 18:06:56 -0500

    added tailwind and navbar with several pages

--- nicks/src/App.js
+++ nicks/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import Header from './components/header';
+// import Header from './components/header';
+import PortfolioContainer from './components/PortfolioContainer';
 
 function App() {
   return (
-    <div className="App">
-        <Header> </Header>
-        
+    <div className="App bg-blue-700">
+        {/* <Header/> */}
+<PortfolioContainer/>
     </div>
   );
 }
--- nicks/src/components/header/index.js
+++ nicks/src/components/header/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import './index.css';
 function Header(){
     return (
-        <h1>Hello World</h1>
+        <h1 className='bg-blue-600'>Hello World</h1>
     )
 }
+
 
 export default Header;
