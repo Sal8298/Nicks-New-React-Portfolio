5136f745d5366e26b3c3e251771ba2282aacf49b
NicholasHM05 <nhmamberger@gmail.com>
2023-03-26 16:50:50 -0500

    added more projects to portfolio with links,etc.

--- nicks/src/components/data/data.js
+++ nicks/src/components/data/data.js
@@ -1,6 +1,10 @@
-//import WorkIm from '../assets/projects/workImg.jpeg'
-//import RealEst from '../assets/projects/realestate.jpg'
+//import WorkIm from '../assets/projects/workImg.jpeg'0
 import sal from '../pages/Assets/sals.png'
+import blog from '../pages/Assets/blog.png'
+import Note from '../pages/Assets/Note.png'
+import jate from '../pages/Assets/jate.png'
+import mysql2 from '../pages/Assets/mysql2.png'
+import oop from '../pages/Assets/oop.png'
 
 export const data=[
     {
@@ -12,24 +16,38 @@ export const data=[
     },
     {
         id:2,
-        name:"React JS Application",
-        image:"",
-        github:"",
-        live:"",
+        name:"MVC Model-View-Controller Application",
+        image:blog,
+        github:"https://github.com/NickHM05/Nick-s-MVC-Tech-Blog",
+        live:"https://sheltered-atoll-65514.herokuapp.com/",
     },
     {
         id:3,
-        name:"React JS Application",
-        image:"",
-        github:"",
-        live:"",
+        name:"Express JS Application",
+        image:Note,
+        github:"https://github.com/NickHM05/Nick-s-NoteTaker-app",
+        live:"https://enigmatic-tundra-41419.herokuapp.com/",
     },
     {
         id:4,
-        name:"React JS Application",
-        image:"",
-        github:"",
-        live:"",
+        name:"PWA (Progressive Web Application) Application",
+        image:jate,
+        github:"https://github.com/NickHM05/Nick-s-Text-Editor",
+        live:"https://mighty-citadel-04566.herokuapp.com/"
+    },
+    {
+        id:5,
+        name:"Object-Oriented Programming Application",
+        image:oop,
+        github:"https://github.com/NickHM05/Nick-s-Team-Profile-Generator",
+        live:"https://drive.google.com/file/d/1fm8bTyav3yd1SXIt6dj83F1TDrKK2Mi9/view"
+    },
+    {
+        id:6,
+        name:"MySQL Application",
+        image:mysql2,
+        github:"https://github.com/NickHM05/Nick-s-Employee-Tracker",
+        live:"https://drive.google.com/file/d/1_Uu7mdaS79Dw-ECQfpNIjfjJa4krXdN6/view"
     },
 
 
--- nicks/src/components/pages/About.jsx
+++ nicks/src/components/pages/About.jsx
@@ -14,21 +14,22 @@ const About = () => {
           </div>
           <div></div>
         </div>
+        {/* Avatar is located here.  */}
         <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
-          <div className='sm:text-right text-4xl font-bold font-railway'>
+          <div className='sm:text-right ml-28 text-4xl font-bold font-railway'>
             <p>Hi. I'm Nicholas Mamberger, nice to meet you. Please take a look around.</p>
           </div>
           <div></div>
-          <img src={avatar} className='w-full basis-1/3 flex-1 grid-cols-2 gap-8 px-4' alt='Nicholas Mamberger avatar'></img>
-          
+          <img src={avatar} className='w-full flex-1 ml-28 justify-center grid-cols-2 gap-8 px-4' alt='Nicholas Mamberger avatar'></img>
         </div>
 
-          <p className='font-railway mt-1 py-4 max-w-[700px] text-2xl'>I am passionate about building excellent software that improves
-            the lives of those around me. I specialize in creating software
-            for clients ranging from individuals and small-businesses all the
-            way to large enterprise corporations. What would you do if you had
-            a software expert available at your fingertips?</p>
-        
+        <p className='font-railway mt-1 py-4 max-w-[700px] text-2xl'>I am passionate about building great software that improves
+          the lives of those around me. I specialize in creating software
+          for clients ranging from individuals and small-businesses all the
+          way to large enterprise corporations. What would you do if you had
+          such an experienced web devloper available at your fingertips?
+        </p>
+
       </div>
     </div>
   );
